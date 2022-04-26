ad7c5ac64f30c6090438b20ab152f1e178d692e5
thucong <51808157+thucong@users.noreply.github.com>
2022-04-26 11:08:41 +0700

    create calendar and add, delete, edit work in day

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import AddWorkModal from './components/AddWorkModal';
+import Calendar from './components/Calendar';
+import ChangeMonth from './components/ChangeMonth';
+import './styles.css'
+
+function App() {
+  return (
+    <div className="App bg-image">
+      <div className="container">
+        <div className="d-flex justify-content-end">
+          {/* <div className="btn-group mt-5 w-25 " role="group" >
+              <button type="button" className="btn btn-outline-primary display-month">Month</button>
+              <button type="button" className="btn btn-outline-primary display-year">Year</button>
+          </div> */}
+        </div>
+        <div className="list-calendar-months ">
+            <div className="calendar">   
+             <ChangeMonth />   
+             <Calendar />
+             <AddWorkModal />
+            </div>
+        </div>
+      
+      </div>
+      
+
+    </div>
+  );
+}
+
+export default App;
